47534af9815646afeb98229945ea07c7c41678b0
Vladimirova Tetiana <vladimirovatanya1107@gmail.com>
2025-04-30 04:16:04 +0300

    developed home page, header, sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import MovieDetails from './pages/MovieDetails'
-import Search from './pages/Search'
-import Schedule from './pages/Schedule'
-import Favorites from './pages/Favorites'
-import AdminPanel from './pages/AdminPanel'
-import Login from './pages/Login'
-import NotFound from './pages/NotFound'
-import PrivateRoute from './components/PrivateRoute'
-import { Moon, Sun } from 'lucide-react'
+import React, { useEffect, useState } from "react";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header/Header";
+import { Moon, Sun } from "lucide-react";
+import AppRouter from "./router/AppRouter";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('theme') === 'dark')
+  const [darkMode, setDarkMode] = useState(
+    () => localStorage.getItem("theme") === "dark"
+  );
 
   useEffect(() => {
-    const root = document.documentElement
+    const root = document.documentElement;
     if (darkMode) {
-      root.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      root.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-  }, [darkMode])
+  }, [darkMode]);
 
   return (
     <AuthProvider>
-      <Navbar />
+      <Sidebar />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+
       <div className="p-4 bg-white dark:bg-gray-900 min-h-screen transition-colors duration-300">
-        <div className="sticky top-20 flex justify-end">
-          <button
-            onClick={() => setDarkMode((p) => !p)}
-            className="bg-gray-200 dark:bg-gray-700 p-2 rounded"
-          >
-            {darkMode ? <Sun /> : <Moon />}
-          </button>
-        </div>
+       
 
-        <h1 className="text-4xl font-bold text-center text-blue-500 dark:text-blue-300 my-6">
-          Вітаємо в Кіноафіші! 🎬
-        </h1>
-
-        <Routes>
-          {/* PUBLIC */}
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/favorites" element={<Favorites />} />
- 
-          {/* AUTH */}
-          <Route path="/login" element={<Login />} />
- 
-          {/* PRIVATE */}
-          <Route 
-            path="/admin" 
-            element={
-              <PrivateRoute>
-                <AdminPanel />
-              </PrivateRoute>
-            } 
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRouter />
       </div>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { EmptyHeartIcon } from "./icons/empty-heart-icon.component";
 
-function MovieCard({ movie }) {
+function MovieCard({ movie, genres }) {
+  const genreNames = movie.genre_ids
+    .map((id) => genres[id])
+    .filter(Boolean)
+    .join(", ");
+
   return (
-    <Link to={`/movie/${movie.id}`} className="rounded overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 bg-white dark:bg-gray-800 dark:shadow-lg dark:hover:shadow-white-2xl dark:transition-shadow dark:duration-300">
-      <img src={movie.poster} alt={movie.title} className="w-full h-72 object-cover" />
+    <Link
+      to={`/movie/${movie.id}`}
+      className="rounded-[20px] w-[300px] h-[450px] overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 bg-white dark:bg-gray-800 dark:shadow-lg dark:hover:shadow-white-2xl"
+    >
+      <div className="relative">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="w-full h-[300px] object-cover object-center"
+        />
+        <div
+          className="border absolute right-[20px] top-[20px] border-white h-[32px] w-[32px] backdrop-blur-[5.93px] flex items-center justify-center"
+          style={{
+            borderRadius: "8.296px",
+            borderWidth: "0.593px",
+            background:
+              "linear-gradient(99deg, #FFF 3.36%, rgba(255, 255, 255, 0) 238.16%)",
+          }}
+        >
+          <EmptyHeartIcon />
+        </div>
+      </div>
+
       <div className="p-4">
-        <h3 className="text-xl font-bold text-gray-900 dark:text-white mb-1">{movie.title}</h3>
-        <p className="text-gray-700 dark:text-gray-300 text-sm mb-1">Жанр: {movie.genre}</p>
-        <p className="text-yellow-500 font-semibold">★ {movie.rating}</p>
+        <h3 className="text-xl font-bold text-gray-900 dark:text-white mb-1">
+          {movie.title}
+        </h3>
+        <p className="text-gray-700 dark:text-gray-300 text-sm mb-1">
+          {genreNames || "Не вказано"}
+        </p>
+        <p className="text-yellow-500 font-semibold">★ {movie.vote_average}</p>
       </div>
     </Link>
   );
